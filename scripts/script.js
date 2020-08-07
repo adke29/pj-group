f5b18e27bc7c100ae729f1adc296683fd47eed72
Kevin <adrian.kevin@yahoo.com>
2020-08-07 23:44:42 +0800

    update-8/7/20

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,13 @@ document.querySelector('.img-btn').addEventListener('click', function()
 		document.querySelector('.cont').classList.toggle('s-signup')
 	}
 );
+document.querySelector('.img-btn2').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+document.querySelector('.img-btn3').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
